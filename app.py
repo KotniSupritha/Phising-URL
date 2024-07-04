7e85213788a24ddc25c8e8d37fc7ee997e4cde3a
kotni supritha <supritha.kotni@gmail.com>
2024-07-04 07:21:31 +0530

    small test

---
+++ app.py
@@ -0,0 +1,19 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+# Create flask app
+flask_app = Flask(__name__)
+model = pickle.load(open("phising.pkl", "rb"))
+
+@flask_app.route("/")
+def Home():
+    return render_template("index.html")
+
+@flask_app.route("/predict", methods = ["POST"])
+def predict():
+    #code...
+    return render_template("index.html", prediction_text = "The URL is {}".format(prediction))
+
+if __name__ == "__main__":
+    flask_app.run(debug=True)
