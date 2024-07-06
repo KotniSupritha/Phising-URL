eedfc8fcb0b7bd73a3bc29b2b8ae9d3812844150
kotni supritha <supritha.kotni@gmail.com>
2024-07-06 07:40:22 +0530

    commit

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template
 import pickle
 
-# Create flask app
-flask_app = Flask(__name__)
-model = pickle.load(open("phising.pkl", "rb"))
+# Load the model
+with open('phishing.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
 
-@flask_app.route("/")
-def Home():
-    return render_template("index.html")
+app = Flask(__name__)
 
-@flask_app.route("/predict", methods = ["POST"])
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    #code...
-    return render_template("index.html", prediction_text = "The URL is {}".format(prediction))
+    url = request.form['url']
+    
+    # Assuming your model's predict method takes a list of URLs
+    prediction = model.predict([url])[0]
+    
+    # Convert the prediction to a human-readable form
+    result = "Phishing" if prediction == 1 else "Not Phishing"
+    
+    return render_template('index.html', prediction=result)
 
-if __name__ == "__main__":
-    flask_app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
